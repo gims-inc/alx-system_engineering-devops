191e1ff71764cb6d07e8302396c3105de8e3df4a
gims-inc <gabrielmusanga@gmail.com>
2022-12-10 01:14:36 +0300

    create a file -

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 #create a file in /tmp directory
 file{'/tmp/school':
-    ensure  => file,
-    content => 'I love Puppet,
+    content => 'I love Puppet',
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744',
