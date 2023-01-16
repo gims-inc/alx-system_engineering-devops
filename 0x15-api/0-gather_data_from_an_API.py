49ea89bedf7e1e434131d4363f84453759890b51
gims-inc <gabrielmusanga@gmail.com>
2023-01-16 15:27:40 +0300

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,27 +10,27 @@ import requests
 
 
 if __name__ == "__main__":
-	url1 = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
-	userRequest = requests.get(url1)
-	userdata = userRequest.json()
-	username = userdata["name"]
-	userId = userdata["id"]
+    url1 = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    userRequest = requests.get(url1)
+    userdata = userRequest.json()
+    username = userdata["name"]
+    userId = userdata["id"]
 
-	url = "https://jsonplaceholder.typicode.com/todos/"
-	todosRequest = requests.get(url)
-	todosData = todosRequest.json()
-	todos = []
-	total = 0
-	completed = 0
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    todosRequest = requests.get(url)
+    todosData = todosRequest.json()
+    todos = []
+    total = 0
+    completed = 0
 
-	for todo in todosData:
-		if todo["userId"] == userId:
-			total += 1
-			if todo["completed"] is True:
-				completed += 1
-				todos.append(todo["title"])
+    for todo in todosData:
+        if todo["userId"] == userId:
+            total += 1
+            if todo["completed"] is True:
+                completed += 1
+                todos.append(todo["title"])
 
-	print(f'Employee {username} is done with tasks({completed}/{total}:')
-
-	for task in todos:
-		print(f'\t {task}')
+    print(f'Employee {username} is done with tasks({completed}/{total}:')
+    
+    for task in todos:
+        print(f'\t {task}')
