39f007f1306ed431e750521b428c7130a61b27b3
gims-inc <gabrielmusanga@gmail.com>
2023-01-16 16:15:22 +0300

    TH!

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-script that, using this
+Using this
 REST API(https://jsonplaceholder.typicode.com/),
 for a given employee ID,
 returns information about his/her TODO list progress.
